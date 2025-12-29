ee60a01ca701d85ea6b0f89727721de83e29dc17
akaosino0987-boop <akaosino0987@gmail.com>
2025-12-29 21:27:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from telegram.ext import (
     ContextTypes,
 )
 
-TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_USERNAME = "@newanimedaily"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
 
 # -------- Button handler --------
 async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -44,7 +44,7 @@ async def auto_post(context: ContextTypes.DEFAULT_TYPE):
     )
 
 # -------- Main --------
-app = ApplicationBuilder().token(TOKEN).build()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(button_handler))
